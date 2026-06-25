013db8e23c583a9c4970ad902a6d358a4c4fc67f
Vladislav <lyskov@kassy.ru>
2026-06-25 13:33:40 +0500

    warp mode change

--- install.sh
+++ install.sh
@@ -43,11 +43,11 @@ WARP_GEO=(
 # 1) всё через WARP:
 #WARP_MODE="all"
 # 2) через WARP только домены/категории из WARP_GEO:
-WARP_MODE="all"
+#ODE="all"
 # 3) через WARP всё, КРОМЕ доменов/категорий из WARP_GEO:
 #WARP_MODE="all-except"
 # 4) WARP выключен, весь трафик прямым IP сервера:
-#WARP_MODE="off"
+WARP_MODE="off"
 # ───────────────────────────────────────────────────────────────
 
 save_state() {
